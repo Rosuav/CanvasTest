1f0f38258a1ea1ab3d2ffc08aaf4896cab652966
Chris Angelico <rosuav@gmail.com>
2021-08-09 12:05:26 +1000

    Change anchor points to be left

--- index.js
+++ index.js
@@ -1,26 +1,34 @@
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext('2d');
-function build_anchor() {
+function build_element_path(has_parent, child_count) {
 	const path = new Path2D;
 	path.moveTo(0, 0);
 	path.lineTo(200, 0);
 	path.lineTo(200, 30);
-	path.lineTo(50, 30);
-	//path.lineTo(40, 20); //Angled snippet
-	//path.lineTo(30, 30);
-	path.arc(40, 30, 10, 0, Math.PI, true); //Curved snippet
+	if (child_count) { //TODO: Handle multiple children (eg conditionals)
+		path.lineTo(10, 30);
+		path.lineTo(10, 35);
+		path.arc(10, 45, 10, Math.PI * 3 / 2, Math.PI / 2, false);
+		path.lineTo(10, 70);
+		path.lineTo(0, 70);
+	}
 	path.lineTo(0, 30);
+	if (has_parent) { //Object has a connection point on its left edge
+		path.lineTo(0, 25);
+		path.arc(0, 15, 10, Math.PI / 2, Math.PI * 3 / 2, true);
+	}
 	path.closePath();
 	return path
 }
 
 const objects = {
-	anchor: build_anchor(),
+	anchor: build_element_path(0, 1),
+	text: build_element_path(1, 0),
 }
 
 const elements = [
 	{type: "anchor", x: 10, y: 10},
-	{type: "anchor", x: 10, y: 80},
+	{type: "text", x: 10, y: 100},
 ];
 
 function draw_at(ctx, el) {
