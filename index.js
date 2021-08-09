3906d43a1dbc1f141b7ba4f79e2e6103ea6d114e
Chris Angelico <rosuav@gmail.com>
2021-08-09 10:47:06 +1000

    Create reusable paths instead of just drawing directly

--- index.js
+++ index.js
@@ -1,20 +1,29 @@
 const canvas = document.querySelector("canvas");
-function draw_anchor(ctx, x, y) {
-	ctx.beginPath();
-	ctx.moveTo(x, y);
-	ctx.lineTo(x + 200, y);
-	ctx.lineTo(x + 200, y + 30);
-	ctx.lineTo(x + 50, y + 30);
-	//ctx.lineTo(x + 40, y + 20);
-	//ctx.lineTo(x + 30, y + 30);
-	ctx.arc(x + 40, y + 30, 10, 0, Math.PI, true);
-	ctx.lineTo(x, y + 30);
-	ctx.closePath();
-	ctx.stroke();
+function build_anchor() {
+	const path = new Path2D;
+	path.moveTo(0, 0);
+	path.lineTo(200, 0);
+	path.lineTo(200, 30);
+	path.lineTo(50, 30);
+	//path.lineTo(40, 20); //Angled snipped
+	//path.lineTo(30, 30);
+	path.arc(40, 30, 10, 0, Math.PI, true); //Curved snippet
+	path.lineTo(0, 30);
+	path.closePath();
+	return path
+}
+
+function draw_at(ctx, path, x, y) {
+	ctx.save();
+	ctx.translate(x, y);
+	ctx.stroke(path);
+	ctx.restore();
 }
 
 function repaint() {
 	const ctx = canvas.getContext('2d');
-	draw_anchor(ctx, 10, 10);
+	const anchor = build_anchor();
+	draw_at(ctx, anchor, 10, 10);
+	draw_at(ctx, anchor, 10, 80);
 }
 repaint();
