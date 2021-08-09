7b0227e22360353d8760f2f2a0c8863b0f999056
Chris Angelico <rosuav@gmail.com>
2021-08-09 17:37:22 +1000

    Add more element types, and text to distinguish them

--- index.js
+++ index.js
@@ -22,21 +22,31 @@ editing (command, trigger, special, etc). Some anchors will offer information th
 will be configurable (eg triggers).
 */
 
+//A type inherently has 0, 1, or 2 (or maybe more) connection sections (children, defined by attribute name).
+//A path has 1 basis location and 1+ connection points for each section
+//An object has an array of children for each section
+//The basis location plus the offset times the child's index equals the connection point location
+//The connection point should be ignored if the corresponding child is not "".
+
 const SNAP_RANGE = 100; //Distance-squared to permit snapping (25 = 5px radius)
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext('2d');
-function build_element_path(has_parent, child_count) {
+function build_element_path(has_parent, children) {
 	const path = new Path2D;
 	path.moveTo(0, 0);
 	path.lineTo(200, 0);
 	path.lineTo(200, 30);
-	if (child_count) { //TODO: Handle multiple children (eg conditionals)
-		path.lineTo(10, 30);
-		path.lineTo(10, 35);
-		path.arc(10, 45, 10, Math.PI * 3 / 2, Math.PI / 2, false);
-		path.lineTo(10, 70);
-		path.lineTo(0, 70);
+	let y = 30;
+	path.connections = []
+	for (let child_count of children) {
+		path.connections.push({x: 10, y, name: "unknown"})
+		path.lineTo(10, y);
+		path.lineTo(10, y + 5);
+		path.arc(10, y + 15, 10, Math.PI * 3 / 2, Math.PI / 2, false);
+		path.lineTo(10, y + 40);
+		y += 40;
 	}
+	path.lineTo(0, y);
 	path.lineTo(0, 30);
 	if (has_parent) { //Object has a connection point on its left edge
 		path.lineTo(0, 25);
@@ -47,16 +57,20 @@ function build_element_path(has_parent, child_count) {
 }
 
 const objects = {
-	anchor: build_element_path(0, 1),
-	text: build_element_path(1, 0),
+	anchor: build_element_path(0, [[]]),
+	text: build_element_path(1, []),
+	builtin: build_element_path(1, [[]]),
+	conditional: build_element_path(1, [[], []]),
 };
 const connections = {
 	anchor: [{x: 10, y: 30, name: "message"}],
 };
 
 const elements = [
-	{type: "anchor", x: 10, y: 10, fixed: true, color: "#ffff00"},
-	{type: "text", x: 10, y: 100, color: "#77eeee"},
+	{type: "anchor", x: 10, y: 10, fixed: true, color: "#ffff00", label: "When !foo is typed..."},
+	{type: "text", x: 220, y: 30, color: "#77eeee", label: "Hello, world!"},
+	{type: "builtin", x: 250, y: 100, color: "#ee77ee", label: "Get channel uptime"},
+	{type: "conditional", x: 10, y: 150, color: "#7777ee", label: "If..."},
 ];
 
 function draw_at(ctx, el) {
@@ -64,6 +78,9 @@ function draw_at(ctx, el) {
 	ctx.translate(el.x|0, el.y|0);
 	ctx.fillStyle = el.color;
 	ctx.fill(objects[el.type]);
+	ctx.fillStyle = "black";
+	ctx.font = "12px sans";
+	ctx.fillText(el.label || "", 20, 20, 175);
 	ctx.stroke(objects[el.type]);
 	ctx.restore();
 }
@@ -91,7 +108,7 @@ canvas.addEventListener("pointerdown", e => {
 function snap_to_elements(xpos, ypos) {
 	//TODO: Optimize this?? We should be able to check against only those which are close by.
 	for (let el of elements) {
-		for (let conn of connections[el.type] || []) {
+		for (let conn of objects[el.type].connections || []) {
 			const snapx = el.x + conn.x, snapy = el.y + conn.y;
 			if (((snapx - xpos) ** 2 + (snapy - ypos) ** 2) <= SNAP_RANGE)
 				return [snapx, snapy]; //First match locks it in. No other snapping done.
