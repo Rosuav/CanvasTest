24ba83471e000786a3d4becc7dedc0a394d22c06
Chris Angelico <rosuav@gmail.com>
2021-08-18 06:13:01 +1000

    Add provides blocks to all builtins

--- index.js
+++ index.js
@@ -97,6 +97,20 @@ const types = {
 		color: "#ee77ee", children: ["message"], label: el => "Hype train status",
 		params: [{attr: "builtin", values: "hypetrain"}],
 		typedesc: "Get info about a current or recent hype train in this channel",
+		provides: {
+			"{error}": "Normally blank, but can have an error message",
+			"{state}": "A keyword (idle, active, cooldown). If idle, there's no other info; if cooldown, info pertains to the last hype train.",
+			"{level}": "The level that we're currently in (1-5)",
+			"{total}": "The total number of bits or bits-equivalent contributed towards this level",
+			"{goal}": "The number of bits or bits-equivalent to complete this level",
+			"{needbits}": "The number of additional bits required to complete this level (== goal minus total)",
+			"{needsubs}": "The number of Tier 1 subs that would complete this level",
+			"{conductors}": "Either None or a list of the current conductors",
+			"{subs_conduct}": "Either Nobody or the name of the current conductor for subs",
+			"{bits_conduct}": "Either Nobody or the name of the current conductor for bits",
+			"{expires}": "Minutes:Seconds until the hype train runs out of time (only if Active)",
+			"{cooldown}": "Minutes:Seconds until the next hype train can start (only if Cooldown)",
+		},
 	},
 	builtin_giveaway: {
 		color: "#ee77ee", children: ["message"], label: el => "Giveaway tools",
@@ -105,21 +119,39 @@ const types = {
 			{attr: "builtin_param", label: "Action", values: ["refund", "status"]},
 		],
 		typedesc: "Handle giveaways via channel point redemptions",
+		provides: {
+			"{error}": "Error message, if any",
+			"{action}": "Action taken - same as subcommand, or 'none' if there was nothing to do",
+			"{tickets}": "Number of tickets you have (or had)",
+		},
 	},
 	builtin_mpn: {
 		color: "#ee77ee", children: ["message"], label: el => "Multi-Player Notepad",
-		params: [{attr: "builtin", values: "mpn"}, {attr: "builtin_param", label: "Action"}], //Not currently editable. Needs a lot of work.
+		params: [{attr: "builtin", values: "chan_mpn"}, {attr: "builtin_param", label: "Action"}], //Not currently editable. Needs a lot of work.
 		typedesc: "Manipulate MPN documents. Not well supported yet.",
+		provides: {
+			"{error}": "Error message, if any",
+			"{action}": "Action performed (if any)",
+			"{url}": "URL to the manipulated document, blank if no such document",
+		},
 	},
 	builtin_pointsrewards: {
 		color: "#ee77ee", children: ["message"], label: el => "Points Rewards",
-		params: [{attr: "builtin", values: "pointsrewards"}, {attr: "builtin_param", label: "Action"}], //Not currently editable. Needs reward ID and a set of commands. Might not be worth doing properly.
+		params: [{attr: "builtin", values: "chan_pointsrewards"}, {attr: "builtin_param", label: "Action"}], //Not currently editable. Needs reward ID and a set of commands. Might not be worth doing properly.
 		typedesc: "Manipulate channel point rewards",
+		provides: {
+			"{error}": "Error message, if any",
+			"{action}": "Action(s) performed, if any (may be blank)",
+		},
 	},
 	builtin_transcoding: {
 		color: "#ee77ee", children: ["message"], label: el => "Transcoding",
 		params: [{attr: "builtin", values: "transcoding"}],
 		typedesc: "Check whether the channel has transcoding (quality options)",
+		provides: {
+			"{resolution}": "Source resolution eg 1920x1080",
+			"{qualities}": "Comma-separated list of additional qualities - if blank, no transcoding",
+		},
 	},
 	builtin_other: {
 		color: "#ee77ee", children: ["message"], label: el => "Unknown Builtin: " + el.builtin,
